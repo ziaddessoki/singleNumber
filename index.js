6c6d292ff2caff08d6a2e1ad3ea851fc5a848f12
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-07-22 02:14:45 +0000

    solved with Set

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 //Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 let singleNumber = (arr) =>{
-  
+  let mySet = new Set();
+  for(i=0;i<arr.length;i++){
+    if(!mySet.has(arr[i])){
+      mySet.add(arr[i])
+    }else{mySet.delete(arr[i])}
+  }return mySet
 }
+
+singleNumber([1,2,1,2,3])
